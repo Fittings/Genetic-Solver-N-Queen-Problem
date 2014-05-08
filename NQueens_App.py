1f5b613a79a1285dbe172d4615ec79b1eeb6c94c
Cameron Milsom <Cameron.Milsom@gmail.com>
2014-05-08 12:41:15 +1200

    Updated TODO. Properly seperated the average finding app and the standard app

--- NQueens_App.py
+++ NQueens_App.py
@@ -7,26 +7,5 @@ import NQueens
 queen_count = 1000
 
 eight = NQueens.NQueens(queen_count)
-sum = 0
-count = 0
-stand_count = 0
-stand_sum = 0
-while count <= 10000:
-    if stand_count < queen_count:
-        stand_sum += stand_count
-        stand_count +=1
-    else:
-        stand_count = 1
-    key = eight.generate_position_key()
-    sum += eight.get_position(key)
-    count += 1
-
-
-
-
-average = sum / count
-stand_average = stand_sum / count
-#eight.print_board()
-print 'The average y value was %d' % average
-print 'The standard average y value was %d' % stand_average
+eight.print_board()
 
